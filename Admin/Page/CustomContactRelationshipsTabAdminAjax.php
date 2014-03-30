15f14428e7f7a2d318e7210254e3c7fb3404816f
Antti Kekki <antti.kekki@gmail.com>
2014-03-30 10:48:53 +0300

    Added phpDoc comments

--- Admin/Page/CustomContactRelationshipsTabAdminAjax.php
+++ Admin/Page/CustomContactRelationshipsTabAdminAjax.php
@@ -4,16 +4,21 @@ require_once "CustomContactRelationshipsTabUtil.php";
 require_once "CustomContactRelationshipsTabAdminDAO.php";
 
 /**
-* Ajax request listener for Contact Relationship tab to return Custom field values for relationship.
-* This listener URL is civicrm/ajax/customContactRelationshipsTabAjaxPage. This is configured in menu.xml.
+* Ajax request listener for CustomContactRelationshipsTab Admin page Ajax calls.
+* This listener methods intercept URLs in form civicrm/customContactRelationshipsTab/settings/ajax/*. This is configured in menu.xml.
+* All methods print JSON-response and terminates CiviCRM.
 */
 class Admin_Page_CustomContactRelationshipsTabAdminAjax {
 
   /**
-  * Returns Custom values for all contact relationships. Also returns this extension configurations table content.
-  * Prints JSON-response and terminates CiviCRM. Requires "contactId" GET parameter.
+  * Returns init data required by extension admin page.
+  * Listens URL civicrm/customContactRelationshipsTab/settings/ajax/getInitData.
   *
-  * Printed JSON object contains following fields: relationshipTypeForRelationshipId, customFieldValues, visibleCustomFieldsConfig, relationshipTypeIds and relationshipTypeNameForId.
+  * Printed JSON object contains following fields:
+  * config: all rows from civicrm_customContactRelationshipsTab_config table
+  * customGroups: all custom groups that belong to Relationships
+  * customFieldsForCustomGroups: all custom fields for all custom groups
+  * relationshipTypes: Relationship types with id and name
   */
   public static function getInitData() {
     $result = array();
@@ -26,16 +31,34 @@ class Admin_Page_CustomContactRelationshipsTabAdminAjax {
     CRM_Utils_System::civiExit();
   }
   
+  /**
+  * Returns all rows from civicrm_customContactRelationshipsTab_config table.
+  * Listens URL civicrm/customContactRelationshipsTab/settings/ajax/getConfig.
+  */
   public static function getConfig() {
     echo json_encode(CustomContactRelationshipsTabUtil::getRelationshipVisibleCustomFieldsFromConfig());
     CRM_Utils_System::civiExit();
   }
   
+  /**
+  * Saves (creates or updates) configuration row in civicrm_customContactRelationshipsTab_config table.
+  * Prints "ok" if save was succesfull. All other responses are error messages.
+  * Listens URL civicrm/customContactRelationshipsTab/settings/ajax/saveConfigRow.
+  *
+  * Saved parameters are queried from $_GET.
+  */
   public static function saveConfigRow() {
     echo CustomContactRelationshipsTabAdminDAO::saveConfigRow($_GET);
     CRM_Utils_System::civiExit();
   }
   
+  /**
+  * Deletes configuration row from civicrm_customContactRelationshipsTab_config table.
+  * Prints "ok" if delete was succesfull.
+  * Listens URL civicrm/customContactRelationshipsTab/settings/ajax/deleteConfigRow.
+  *
+  * Saved parameters are queried from $_GET.
+  */
   public static function deleteConfigRow() {
     CustomContactRelationshipsTabAdminDAO::deleteConfigRow($_GET);
     
